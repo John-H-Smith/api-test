1ae36dc31a4d9fdaf6d2691e337b0ae38929ab75
John-H-Smith <derFabi95@gmail.com>
2022-01-12 10:40:40 +0100

    Added ids to json api

--- routes/simple-json-api.js
+++ routes/simple-json-api.js
@@ -20,13 +20,39 @@ router.post( '/', async ( req, res ) => {
         return;
     }
 
-    entries.push( { name: req.body.name } );
+    if( req.body.id == null || req.body.id == '' ) {
+        res.status( 400 ).end( "Please provide an id!" );
+        return;
+    }
+
+    if( isNaN( req.body.id ) ) {
+        res.status( 400 ).end( "id has to be a number!" );
+        return;
+    }
+
+    req.body.id = Number( req.body.id );
+
+    const entry = entries.find( e => e.id === req.body.id );
+    if( entry != null ) {
+        res.status( 409 ).end( "id already exists!" );
+        return;
+    }
+
+    entries.push( { name: req.body.name, id: req.body.id } );
     res.status( 201 ).end();
 });
 
-// Get single entry by 'name'
-router.get( '/:name', async ( req, res ) => {
-    const entry = entries.find( e => e.name === req.params.name );
+// Get single entry by 'id'
+router.get( '/:id', async ( req, res ) => {
+
+    if( isNaN( req.params.id ) ) {
+        res.status( 400 ).end( "id has to be a number!" );
+        return;
+    }
+
+    req.params.id = Number( req.params.id );
+
+    const entry = entries.find( e => e.id === req.params.id );
     if( entry == null ) {
         res.status( 404 ).end();
         return;
@@ -36,25 +62,40 @@ router.get( '/:name', async ( req, res ) => {
 });
 
 // Delete an entry by 'name'
-router.delete( '/:name', async ( req, res ) => {
-    const entry = entries.find( e => e.name === req.params.name );
+router.delete( '/:id', async ( req, res ) => {
+
+    if( isNaN( req.params.id ) ) {
+        res.status( 400 ).end( "id has to be a number!" );
+        return;
+    }
+
+    req.params.id = Number( req.params.id );
+
+    const entry = entries.find( e => e.id === req.params.id );
     if( entry == null ) {
         res.status( 404 ).end();
         return;
     }
 
-    entries = entries.filter( e => e.name !== req.params.name );
+    entries = entries.filter( e => e.id !== req.params.id );
     res.status( 200 ).end();
 });
 
 // Update an entry by 'name'
-router.put( '/:name', async ( req, res ) => {
+router.put( '/:id', async ( req, res ) => {
     if( req.body.name == null || req.body.name == '' ) {
         res.status( 400 ).end( "Please provide a name!" );
         return;
     }
 
-    let entry = entries.find( e => e.name === req.params.name );
+    if( isNaN( req.params.id ) ) {
+        res.status( 400 ).end( "id has to be a number!" );
+        return;
+    }
+
+    req.params.id = Number( req.params.id );
+
+    let entry = entries.find( e => e.id === req.params.id );
     if( entry == null ) {
         res.status( 404 ).end();
         return;
